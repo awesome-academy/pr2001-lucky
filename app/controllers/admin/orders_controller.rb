7beef35b53b7adbebcbea01a2409dfd10d4d27ac
Your Name <truongvietbach10a1k64@gmail.com>
2020-07-20 20:44:42 +0700

    update_2

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,18 +1,5 @@
 # frozen_string_literal: true
 
 class Admin::OrdersController < Admin::BaseController
-  before_action :get_value_from_user
-  def index
-    @orders = @user.order
-  end
 
-  def show
-    @order = @user.orders.find(params[:id])
-  end
-
-  private
-
-  def get_value_from_user
-    @user = User.find(params[:user_id])
-  end
 end
